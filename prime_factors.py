7557d9e0bc1d2cc8b7da988879a3672b5b677f17
yunspark <yun.park.kor@gmail.com>
2025-07-31 12:39:10 +0900

    [feature] 34

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,8 @@
 class PrimeFactor:
-    pass
+    def of(self, number) -> []:
+        factors = []
+        if number == 2:
+            factors.append(2)
+        elif number == 3:
+            factors.append(3)
+        return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -2,4 +2,12 @@ from prime_factors import PrimeFactor
 
 def test_prime_factor_of_1():
     prime_factor = PrimeFactor()
-    assert 1 == 1
+    assert prime_factor.of(1) == []
+
+def test_prime_factor_of_2():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(2) == [2]
+
+def test_prime_factor_of_3():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(3) == [3]
