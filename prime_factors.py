60421c0de64e4202dccf364b0710127ba730abda
yp1024 <yun.park.ml@gmail.com>
2025-07-31 13:06:52 +0900

    Update prime_factors.py
    if number 부분 refactoring 완료하였습니다

--- prime_factors.py
+++ prime_factors.py
@@ -2,14 +2,10 @@ class PrimeFactor:
     def of(self, number: int) -> list[int]:
         factors = []
         divisor = 2
-        if number == 4 or number == 6 or number == 9 or number == 12:
-            divisor = 2
-            while number > 1:
-                while number % divisor == 0:
-                    factors.append(divisor)
-                    number //= divisor
-                divisor += 1
-        else:
-            factors.append(number)
+        while number > 1:
+            while number % divisor == 0:
+                factors.append(divisor)
+                number //= divisor
+            divisor += 1
 
         return factors
