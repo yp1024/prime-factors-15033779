a9488047007fa3d0ecdba30ce0235cd130c94070
yunspark <yun.park.kor@gmail.com>
2025-07-31 12:40:23 +0900

    [feature] 36

--- prime_factors.py
+++ prime_factors.py
@@ -1,8 +1,6 @@
 class PrimeFactor:
     def of(self, number) -> []:
         factors = []
-        if number == 2:
-            factors.append(2)
-        elif number == 3:
-            factors.append(3)
+        if number > 1:
+            factors.append(number)
         return factors
